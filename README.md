4580c3c1d114fc2529dc762b7f62039386848819
Amy Shafer <amy.u.shafer@gmail.com>
2021-12-16 23:54:53 -0500

    attempted to add validation capabilities although the code is not fully funcitonal

---
+++ README.md
@@ -0,0 +1,50 @@
+
+# Password Generator
+
+This is a Password Generator. It creates a unique and secure password based on a user's inputs.
+
+This project was built upon from existing code. The present HTML and CSS code is nearly-identical to the code originally provided. Colors and font-families have changed but sizing and positioning were untouched.
+
+Some JavaScript was given at the start of the project. Over 80 lines of original code were added.
+
+## Table of contents
+* [Technologies](#technologies)
+* [Mockup](#mockup)
+* [Resources-used](#resources-used)
+* [Possible-improvements](#possible-improvements)
+
+## Technologies
+
+This website was built and refactored using HTML, CSS, and JavaScript.
+
+Users do not need setup.
+
+## Mockup
+
+The following image shows the website is action!
+
+<img src="./assets/passwordGenerator.jpeg"/>
+
+## Resources-used
+
+* <a href="https://replit.com/repls/folder/Penn%20LPS" target="_blank_">Repl creations that helped me draft the final product.</a> 
+
+* <a href="https://github.com/AmyShafer/decemberFolder" target="_blank_">A Practice Project that served as my training wheels.</a> 
+
+* <a href="https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/Events" target="_blank_">I read up on JavaScript Events.</a> 
+
+* <a href="https://stackoverflow.com/questions/3943772/how-do-i-shuffle-the-characters-in-a-string-in-javascript" target="_blank_">This code helped me produce my own shuffler.</a> 
+
+* <a href="https://w3schools.sinsixx.com/js/js_special_characters.asp.htm" target="_blank_">I required additional assistance when dealing with special characters.</a> 
+
+
+## Future-improvements
+
+The application's randomizer could be more...random. Even the the different characters types are random, the types are clustered. In another words, typically all the lowercase letters seems to be together, all the uppercase together, and so on.
+
+Presently, the applications validation testing is not fully functional. For instance, even though the application is able to test is the user enter a valid password length, the new password length is not update.
+
+Similarly, even though the application is able to test whether or not the user did not enter any of the needed character selections, the code that follows is broken. 
+
+
+
--- assets/script.js
+++ assets/script.js
@@ -7,16 +7,23 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.innerText = password;
-
 }
 
 function generatePassword() {
-  var length = prompt("Please pick a length at least 8 characters and no more than 128 characters.");
-  var str = charOptions(charTypes());
+  var length = prompt("Please pick a length of least 8 characters and no more than 128 characters.");
+  var response = userAnswerArray(userAnswersPrompts());
+  var inputValidation = validateInput(length, response);
+  
+  if (!inputValidation.validInput) {
+    length = inputValidation.validation.length;
+    response = inputValidation.validation.arr;
+  }
+
+  var str = charOptions(response);
   var newPassword = [];
   var splitStr = str.split("");
 
-  // if the string pf possible chars is shorter than
+  // if the string of possible chars is shorter than
   // the password length the user selected
   // repeat the possible char arr 
   if (splitStr.length < length) {
@@ -39,63 +46,74 @@ function generatePassword() {
   return newPassword.join("");
 }
 
-// This functions stores the User's Input
-function charTypes() {
-  var lowerCase = prompt("If you would like to included lowercase characters, type YES");
-  var upperCase = prompt("If you would like to included uppercase characters, type YES");
-  var numbers = prompt("If you would like to included numeric characters, type YES");
-  var specialChars = prompt("If you would like to included special characters, type YES");
-    
-  // object that stores the character possiblities
-  var charChoices = {
-    lowerCase: false,
-    upperCase: false, 
-    numbers: false,
-    specialChars: false
+// This functions stores the User's Input as an object
+function userAnswersPrompts () {
+  var userInput = {
+    lowerCase: prompt("If you would like to included lowercase characters, type Y"),
+    upperCase: prompt("If you would like to included uppercase characters, type Y"),
+    numbers: prompt("If you would like to included numeric characters, type Y"),
+    specialChars: prompt("If you would like to included special characters, type Y")
   }
 
-  // this makes all the user prompt uppercase
-  var promptInputs = [lowerCase, upperCase, numbers, specialChars];
-  var allUpperInput = [];
-  promptInputs.forEach(x => {
-    allUpperInput.push(x.toUpperCase())
-  }); 
+  return userInput;
+}
 
-  // if user prompt value is YES, change the value to true 
-  if (allUpperInput[0] === "YES") {
-    charChoices.lowerCase = true;
-  } 
-  /// if user prompt value is YES, change the value to true 
-  if (allUpperInput[1] === "YES") {
-    charChoices.upperCase = true;
-  } 
-  // if user prompt value is YES, change the value to true 
-  if (allUpperInput[2] === "YES") {
-    charChoices.numbers = true;
-  } 
-  // if user prompt value is YES, change the value to true 
-  if (allUpperInput[3] === "YES") {
-    charChoices.specialChars = true;
-  } 
-  
-  console.log(charChoices);
-  return charChoices;
+// This function turns the user prompts into an array
+function userAnswerArray (userInputObj) {
+
+  var upperCaseInput = [];
+
+  Object.values(userInputObj).forEach(x => {
+    upperCaseInput.push(x.toUpperCase());
+  });
+
+  return upperCaseInput;
 }
 
 // This function compiles the possible characters to use based on the users answers
-function charOptions (charObj) {
-  var charArr = ["abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIKLMNOPQRSTUVWXYZ", "0123456789", "!+-"];
+function charOptions (userAnswerArray) {
+  var charArr = ["abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIKLMNOPQRSTUVWXYZ", "0123456789", " !\"#$%\&\'()*+,-./:;<=>?@[\\]^_`{|}~"];
 
   var possibleChars = "";
 
-  Object.values(charObj).map((key, i) => {
-    if (key) {
+  userAnswerArray.forEach((x, i) => {
+    if (x === "Y") {
       possibleChars += charArr[i];
     }
   });
-
+  
+  // return a string of possible characters to use
   return possibleChars;
 }
+
+// This function is a validation test
+function validateInput(checkLength, checkArray) {
+  var validation = {
+    validInput: false,
+    length: checkLength,
+    arr: checkArray
+  }
+
+  if (checkLength > 7 && checkLength  < 128) {
+    validation.length = checkLength;
+    validation.validInput = true;
+  } else {
+    validation.length = prompt("Please enter a valid character length. Any number over 7 or under 129");  
+    validation.validInput = false;
+  }
+
+  if (checkArray.includes("Y")) {
+    validation.arr = checkArray;
+    validation.validInput = true;
+  } else {
+    validation.arr = prompt("You need to pick at least one character type.");
+    charOptions(userAnswers());
+    validation.validInput = false;
+  }
+
+  return validation;
+}
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword());
