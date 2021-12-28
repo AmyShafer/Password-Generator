8b46a9769bdb3da6d6abc3f102e086a98fe0f4a3
Amy Shafer <amy.u.shafer@gmail.com>
2021-12-28 01:30:19 -0500

    corrections to validation

--- assets/script.js
+++ assets/script.js
@@ -1,53 +1,25 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.innerText = password;
-}
-
 function generatePassword() {
   var length = prompt("Please pick a length of least 8 characters and no more than 128 characters.");
-  var response = userAnswerArray(userAnswersPrompts());
-  var inputValidation = validateInput(length, response);
-  
-  if (!inputValidation.validInput) {
-    length = inputValidation.validation.length;
-    response = inputValidation.validation.arr;
+  var str = "";
+
+  if (length > 7 && length < 138) {
+    var response = userAnswerArray(userAnswersPrompts());
+    str = charOptions(response, length);  
+  } else {
+    //generatePassword();
+    var length = prompt("Please pick a length of least 8 characters and no more than 128 characters.");
+    var response = userAnswerArray(userAnswersPrompts());
+    str = charOptions(response, length);  
   }
 
-  var str = charOptions(response);
-  var newPassword = [];
-  var splitStr = str.split("");
-
-  // if the string of possible chars is shorter than
-  // the password length the user selected
-  // repeat the possible char arr 
-  if (splitStr.length < length) {
-    var lengthDifference = length - splitStr.length;
-    for (var i = 0; i < lengthDifference; i++) {
-      var currentLetter = [...splitStr[i]];
-      splitStr.push(currentLetter);
-    }
-  }
-
-  // loop through the array of possible char and grab
-  // random chars from the list
-  for (var i = 0; i < length; i++) {
-    var j = Math.floor(Math.random() * (i + 1));
-    var currentChar = splitStr[j];
-    newPassword.push(currentChar);
-  }
-
-  // return newPassword and join the array into a string
-  return newPassword.join("");
+  return str;
 }
 
 // This functions stores the User's Input as an object
-function userAnswersPrompts () {
+function userAnswersPrompts() {
   var userInput = {
     lowerCase: prompt("If you would like to included lowercase characters, type Y"),
     upperCase: prompt("If you would like to included uppercase characters, type Y"),
@@ -59,7 +31,7 @@ function userAnswersPrompts () {
 }
 
 // This function turns the user prompts into an array
-function userAnswerArray (userInputObj) {
+function userAnswerArray(userInputObj) {
 
   var upperCaseInput = [];
 
@@ -70,51 +42,33 @@ function userAnswerArray (userInputObj) {
   return upperCaseInput;
 }
 
-// This function compiles the possible characters to use based on the users answers
-function charOptions (userAnswerArray) {
+// This function compiles the possible characters to use based on the user's answers
+function charOptions(userAnswerArray, passwordLength) {
   var charArr = ["abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIKLMNOPQRSTUVWXYZ", "0123456789", " !\"#$%\&\'()*+,-./:;<=>?@[\\]^_`{|}~"];
-
   var possibleChars = "";
+  var finalCharOptions = "";
 
   userAnswerArray.forEach((x, i) => {
     if (x === "Y") {
       possibleChars += charArr[i];
     }
   });
-  
-  // return a string of possible characters to use
-  return possibleChars;
+
+  for (var i = 0; i < passwordLength; i++) {
+    const randomIndex = Math.floor(Math.random() * possibleChars.length);
+    finalCharOptions += possibleChars[randomIndex]
+  }
+
+  return finalCharOptions;
 }
 
-// This function is a validation test
-function validateInput(checkLength, checkArray) {
-  var validation = {
-    validInput: false,
-    length: checkLength,
-    arr: checkArray
-  }
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-  if (checkLength > 7 && checkLength  < 128) {
-    validation.length = checkLength;
-    validation.validInput = true;
-  } else {
-    validation.length = prompt("Please enter a valid character length. Any number over 7 or under 129");  
-    validation.validInput = false;
-  }
-
-  if (checkArray.includes("Y")) {
-    validation.arr = checkArray;
-    validation.validInput = true;
-  } else {
-    validation.arr = prompt("You need to pick at least one character type.");
-    charOptions(userAnswers());
-    validation.validInput = false;
-  }
-
-  return validation;
+  passwordText.innerText = password;
 }
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword());
-
